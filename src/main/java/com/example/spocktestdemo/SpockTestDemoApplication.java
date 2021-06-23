c4784aa0829d0bd5b0d35601b3528eea45f2b719
gary <gary.jin@aylaasia.com>
2021-06-23 10:06:03 +0800

    init repo

---
+++ src/main/java/com/example/spocktestdemo/SpockTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spocktestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpockTestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spocktestdemo/SpockTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spocktestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
